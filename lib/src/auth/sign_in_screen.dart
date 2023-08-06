7c8b0162d8dc2c550d9f35de3acf095d19c86f5f
Anderson Vallada Viana <anderson.viana@arcadebeauty.com>
2023-08-06 16:59:54 -0300

    first commit - Login Screen

---
+++ lib/src/auth/sign_in_screen.dart
@@ -0,0 +1,168 @@
+import 'package:animated_text_kit/animated_text_kit.dart';
+import 'package:flutter/material.dart';
+import 'package:greengrocer/src/auth/components/custom_text_field.dart';
+import 'package:greengrocer/src/config/custom_colors.dart';
+
+class SignInScreen extends StatelessWidget {
+  const SignInScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: CustomColors.customSwatchColor,
+      body: Column(children: [
+        Expanded(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              //App Name
+              Text.rich(
+                TextSpan(
+                    style: const TextStyle(
+                      fontSize: 40,
+                    ),
+                    children: [
+                      const TextSpan(
+                        text: 'Green',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      TextSpan(
+                        text: 'grocer',
+                        style: TextStyle(
+                          color: CustomColors.customContrastColor,
+                        ),
+                      )
+                    ]),
+              ),
+
+              SizedBox(
+                height: 30,
+                child: DefaultTextStyle(
+                  style: const TextStyle(fontSize: 25),
+                  child: AnimatedTextKit(
+                    pause: Duration.zero,
+                    repeatForever: true,
+                    animatedTexts: [
+                      FadeAnimatedText('Frutas'),
+                      FadeAnimatedText('Verduras'),
+                      FadeAnimatedText('Legumes'),
+                      FadeAnimatedText('Carnes'),
+                      FadeAnimatedText('Cereais'),
+                      FadeAnimatedText('Laticíneos'),
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+        //Form
+        Container(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 32,
+            vertical: 40,
+          ),
+          decoration: const BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.vertical(
+              top: Radius.circular(45),
+            ),
+          ),
+          child:
+              Column(crossAxisAlignment: CrossAxisAlignment.stretch, children: [
+            //email
+            const CustomTextField(
+              icon: Icons.email,
+              label: 'Email',
+            ),
+            //password
+            const CustomTextField(
+              icon: Icons.lock,
+              label: 'Senha',
+              isSecret: true,
+            ),
+            SizedBox(
+              height: 50,
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.green,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(18),
+                  ),
+                ),
+                //Enter
+                onPressed: () {},
+                child: const Text(
+                  'Entrar',
+                  style: TextStyle(
+                    fontSize: 18,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+            //Forget Password
+            Align(
+              alignment: Alignment.centerRight,
+              child: TextButton(
+                onPressed: () {},
+                child: Text(
+                  'Esqueceu a senha?',
+                  style: TextStyle(color: CustomColors.customContrastColor),
+                ),
+              ),
+            ),
+            //Divider
+            Padding(
+              padding: const EdgeInsets.only(bottom: 10),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Divider(
+                      color: Colors.grey.withAlpha(90),
+                      thickness: 2,
+                    ),
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+                    child: Text('ou'),
+                  ),
+                  Expanded(
+                    child: Divider(
+                      color: Colors.grey.withAlpha(90),
+                      thickness: 2,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            //New User
+            SizedBox(
+              height: 50,
+              child: OutlinedButton(
+                style: OutlinedButton.styleFrom(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(18)),
+                  side: const BorderSide(
+                    width: 2,
+                    color: Colors.green,
+                  ),
+                ),
+                onPressed: () {},
+                child: const Text(
+                  'Criar Conta',
+                  style: TextStyle(
+                    fontSize: 18,
+                  ),
+                ),
+              ),
+            ),
+          ]),
+        ),
+      ]),
+    );
+  }
+}
